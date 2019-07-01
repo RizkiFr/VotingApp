b76056ccfff52e25893bc574dfb2fc10a07165b6
rizkifr <rizqifauzi12@gmail.com>
2019-07-01 11:00:01 +0700

    last commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex v-for="(vote, id) in votes" :key="id" md6 lg6 xs12>
+        <v-card flat class="text-xs-center ma-3">
+          <v-card-text>
+            <v-avatar v-if="vote.id==1" size="300">
+              <img src="1.png">
+            </v-avatar>
+            <v-avatar v-if="vote.id==2" size="300">
+              <img src="2.png">
+            </v-avatar>
+            <div class="subheading mt-3"><span class="text-weight-bold title">{{vote.nama}}</span></div>
+            <div class="grey--text">Capres &amp; Cawapres No. 0{{vote.id}}</div>
+          </v-card-text>
+          <v-btn block v-on:click="addVote(vote.id)" class="cyan">Vote</v-btn>
+        </v-card>
+          <v-card flat dark class="ma-3 red justify-end">
+            <v-layout text-xs-center>
+             <v-card-text class="title">{{vote.voting}} Voters</v-card-text>
+            </v-layout>
+          </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  nama: 'vote',
+  apikey:'AIzaSyBpOsUshWJTOEUe3B0cD97KWRiqWQctuTA',
+  idkey:'UChF7Xq30UeZEARi_29YVrvg',
+  data(){
+    return{
+      votes:[]
+    }
+  },
+  methods:{
+    async getList() {
+          this.axios.get('http://api.sinetral.online/public/vote')
+          .then((response) => {(this.votes = response.data)
+          })
+        },
+
+    addVote: function(id){
+      this.axios.put('http://api.sinetral.online/public/vote/'+id).then((response)=>{
+      this.getList(response)
+      })
+    },
+  },
+    mounted () {
+    this.getList()
+    setInterval(() => this.getList(), 300000); 
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
